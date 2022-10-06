5dbbc4e0788ae185e0559dcc2e072d4e6c6d8f5c
nst040302 <89286105+nst040302@users.noreply.github.com>
2022-10-06 17:35:50 +0700

    Setup Client structure folders

---
+++ client/src/pages/Home/HomeBanner.js
@@ -0,0 +1,47 @@
+import { Pagination, A11y } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+
+const HomeBanner = () => {
+    return (
+        <Swiper
+            modules={[Pagination, A11y]}
+            spaceBetween={0}
+            slidesPerView={1}
+            pagination={{ clickable: true }}
+        >
+            <SwiperSlide>
+                <div
+                    style={{
+                        height: '570px',
+                        backgroundImage: `url("https://img.ltwebstatic.com/images3_ach/2022/09/16/16633244073de9d73cee669be190a121a553839126.jpg")`,
+                        backgroundPosition: 'center',
+                        backgroundSize: 'cover'
+                    }}
+                    className="w-full"
+                >
+                </div>
+            </SwiperSlide>
+            <SwiperSlide>
+                <div
+                    style={{
+                        height: '570px',
+                        backgroundImage: `url("https://img.ltwebstatic.com/images3_ach/2022/09/09/16626907321167ba0f84252e89c38dc5aa3c47e311.gif")`,
+                        backgroundPosition: 'center',
+                        backgroundSize: 'cover'
+                    }}
+                    className="w-full"
+                >
+                </div>
+            </SwiperSlide>
+        </Swiper>
+    )
+}
+
+export default HomeBanner
