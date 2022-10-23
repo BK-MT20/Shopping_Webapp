5b5c1000049a26a38b595cff108e44f66abde74e
HoangPhiem <tuhoangphiem10@gmail.com>
2022-10-23 23:58:25 +0700

    Complete Add,Edit and Delete function with real API

--- admin/src/components/productHeader/index.js
+++ admin/src/components/productHeader/index.js
@@ -2,17 +2,20 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Layout, PageHeader } from 'antd'
 import BreadCrumb from '../breadCrumb'
+import { useLocation } from 'react-router-dom'
+
 const ProductHeader = ({ title, subtitle }) => {
+  const location = useLocation()  
+  const contain = location.pathname.includes('Add' || 'Edit')
+  // console.log(contain)
   return (
     <Layout className='page-header'>
       <BreadCrumb />
       <PageHeader
         className="site-page-header"
         title={title}
-        // breadcrumb={{
-        //   routes,
-        // }}
-        subTitle={subtitle}
+
+        subTitle={contain ? '' : subtitle }
       />
     </Layout>
   )
---
+++ server/src/app/routes/product.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const router = express.Router()
+const product = require('../controllers/product/product.controller')
+
+
+router.get('/getAllProduct/',product.getAllProduct)
+router.post('/createProduct',product.createProduct)
+router.get('/:id',product.getById)
+router.put('/:id',product.updateProduct)
+router.delete('/:id',product.deleteProduct)
+
+
+
+
+module.exports = router
