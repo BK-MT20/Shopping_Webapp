d8998edc780c6cddc56ffea07309ba210763eae5
ahop281 <drakenahop281@gmail.com>
2022-10-26 21:29:02 +0700

    [AO-5] Create Orders Notification

--- admin/src/components/header/index.js
+++ admin/src/components/header/index.js
@@ -1,7 +1,8 @@
 import { Layout, Typography, Dropdown, Menu, Space, Divider } from 'antd'
-import React from 'react'
+import React, { memo } from 'react'
 import { UserOutlined, LogoutOutlined } from '@ant-design/icons'
 import './style.css'
+import Notification from '../notification'
 const menu = (
   <Menu
     items={[
@@ -29,13 +30,14 @@ function Header() {
   return (
     <Layout.Header  style ={{ display:'flex', justifyContent: 'space-between', alignItems:'center' }}className="header">
       <Typography.Title style={{ color: 'white' }}>Shopping Webapp</Typography.Title>
-      <Dropdown overlay={menu}>
-        <UserOutlined style={{ color:'white', fontSize:'16px' }} />
-
-      </Dropdown>
-
+      <Space size={'large'}>
+        <Notification />
+        <Dropdown overlay={menu}>
+          <UserOutlined style={{ color:'white', fontSize:'16px' }} />
+        </Dropdown>
+      </Space>
     </Layout.Header>
   )
 }
 
-export default Header
+export default memo(Header)
