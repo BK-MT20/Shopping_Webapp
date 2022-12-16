aa24502f13ccf35bb296b52e9b5d485f4f416e97
HoangPhiem <tuhoangphiem10@gmail.com>
2022-12-16 17:48:53 +0700

    AL-2 add role admin for signin/signup and middleware for admin api

--- admin/src/services/Auth.js
+++ admin/src/services/Auth.js
@@ -1,53 +1,54 @@
-import axios from '../api'
-import { message } from 'antd'
+import axios from "../api";
+import { message } from "antd";
 
 const login = (values) => {
   return axios
-    .post('/auth/signin', {
+    .post("/auth/signin", {
       username: values.username,
       password: values.password,
+      role: "admin",
     })
     .then((res) => {
-      console.log(res)
+      console.log(res);
       if (res.data.accessToken) {
-        localStorage.setItem('admin', JSON.stringify(res.data))
+        localStorage.setItem("admin", JSON.stringify(res.data));
       }
     })
     .catch((err) => {
-      console.log(err)
+      console.log(err);
       if (err.response.status === 500) {
-        message.error('Internal Server Error')
+        message.error("Internal Server Error");
       }
       if (err.response.status === 401) {
-        message.error('Password is wrong')
+        message.error("Password is wrong");
       }
       if (err.response.status === 404) {
-        message.error('User not found')
+        message.error("User not found");
       }
-    })
-}
+    });
+};
 
 const logout = () => {
-  localStorage.removeItem('admin')
-}
+  localStorage.removeItem("admin");
+};
 
 const register = (values) => {
-  return axios.post('/auth/signup', {
+  return axios.post("/auth/signup", {
     username: values.username,
     email: values.email,
     password: values.password,
-    role: 'admin'
-  })
-}
+    role: "admin",
+  });
+};
 const getCurrentUser = () => {
-  return JSON.parse(localStorage.getItem('admin'))
-}
+  return JSON.parse(localStorage.getItem("admin"));
+};
 
 const authService = {
   login,
   logout,
   register,
   getCurrentUser,
-}
+};
 
-export default authService
+export default authService;
